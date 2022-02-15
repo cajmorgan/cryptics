75277be435a78dbf30525bc19994a3900e8a651d
cajmorgan <caj@cajmorgan.com>
2022-02-15 22:06:33 +0100

    added some RSA stuff

--- lib/rsa/rsa.c
+++ lib/rsa/rsa.c
@@ -7,22 +7,28 @@
 
 #define E 65537
 
-uint64_t cryptics_rsa_32_generate_keys() {
-  uint64_t rand_p, rand_q, prime_product, totient_product, private_exp;
+int cryptics_rsa_32_generate_keys(struct rsa_32_public_key *public_key, struct rsa_32_private_key *private_key) {
+  uint64_t rand_p, rand_q, modulus, totient_product, private_exp;
   rand_p = cryptics_primes_random_prime(196611, 100000);
   rand_q = cryptics_primes_random_prime(21845, 10000);
 
-  prime_product = rand_p * rand_q;
+  modulus = rand_p * rand_q;
   totient_product = cryptics_general_lcm(rand_p-1, rand_q-1);
   private_exp = cryptics_general_mmi(E, totient_product);
 
   if (private_exp < 0) {
     return -1;
   }
-  printf("P %ld\n", rand_p);
-  printf("Q %ld\n", rand_q);
-  printf("Prime Product: %ld\n", prime_product);
-  printf("LCM: %ld\n", totient_product);
-  printf("PRIVATE %ld\n", private_exp);
-  return rand_p;
+
+  public_key->modulus = modulus;
+  public_key->public_key_exponent = E;
+
+  private_key->version = 1;
+  private_key->modulus = modulus;
+  private_key->public_key_exponent = E;
+  private_key->private_key_exponent = private_exp;
+  private_key->prime_p = rand_p;
+  private_key->prime_q = rand_q;
+
+  return 0;
 }
--- lib/rsa/rsa.h
+++ lib/rsa/rsa.h
@@ -1,6 +1,20 @@
 #ifndef RSA_H_INCLUDED
 #define RSA_H_INCLUDED
 
-  uint64_t cryptics_rsa_32_generate_keys();
+  struct rsa_32_public_key {
+    uint64_t modulus;
+    uint64_t public_key_exponent;
+  };
+
+  struct rsa_32_private_key {
+    int version;
+    uint64_t modulus;
+    uint64_t public_key_exponent;
+    uint64_t private_key_exponent;
+    uint64_t prime_p;
+    uint64_t prime_q;
+  };
+
+  int cryptics_rsa_32_generate_keys(struct rsa_32_public_key *, struct rsa_32_private_key *);
 
 #endif // RSA_H_INCLUDED
